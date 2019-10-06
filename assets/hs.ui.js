440fb2a7b59805ae0eb4ed85cc45ca4c691451a3
Sathish <sathish.2009btech@gmail.com>
2019-10-06 19:06:42 +0530

    implemented local storage

--- assets/hs.ui.js
+++ assets/hs.ui.js
@@ -3,24 +3,69 @@ var healthseeker = new (function() {
 	var self = this;
 
 	function reset() {
-		$("#name").val('');
+		$("#gmi").val('');
 		$("#mobile").val('');
 		$("#age").val('');
 		location.href = '/gapwa/offline_hs_datatable.html';
 	}
 
-	self.save = function() {
-		var name = $("#name").val();
-		var mobile = $("#mobile").val();
+	self.next = function(template) {
+		var govtMotherId = $("#gmi").val();
+		var motherName = $("#mother_name").val();
 		var age = $("#age").val();
+		var contact = $("#contact").val();
+		var occupation = $("#occupation").val();
+		var husbandName = $("#Husband_Name").val();
+		var husbandOccupation = $("#Husband_Occupation").val();
+
+		var data = {};
+		data.govtMotherId = govtMotherId;
+		data.motherName = motherName;
+		data.age = age;
+		data.contact = contact;
+		data.occupation = occupation;
+		data.husbandName = husbandName;
+		data.husbandOccupation = husbandOccupation;
+		var parameters = new URL(window.location).searchParams;
+		var id = parameters.get("id");
+		if(id) {
+			data.id = id;
+			template = template+'?id='+id;
+		}
+		storage.saveHealthSeeker(data);
+		location.href = '/gapwa/'+template;
+	}
+
+	self.goBack = function() {
+		window.history.back();
+	}
+
+	self.save = function() {
+
+		var data = storage.getHealthSeeker();
+
+		var visit_date = $("#visit_date").val();
+		var height = $("#Height").val();
+		var weight = $("#weight").val();
+		var bp = $("#bp").val();
+		var hemoglobin = $("#Hemoglobin").val();
+		var blood_sugar = $("#Blood_sugar").val();
+		var urine_albumin = $("#urine_albumin").val();
 
 		// construct
-		var data = {};
-		data.name = name;
-		data.mobile = mobile;
-		data.age = age;
+		var health_parameter = {};
+		health_parameter.visit_date = visit_date;
+		health_parameter.height = height;
+		health_parameter.weight = weight;
+		health_parameter.bp = bp;
+		health_parameter.hemoglobin = hemoglobin;
+		health_parameter.blood_sugar = blood_sugar;
+		health_parameter.urine_albumin = urine_albumin;
+
+		data.health_parameter = health_parameter;
 		hsPresenter.save(data)
 			.then(res => {
+				storage.resetHealthSeeker();
 				alert("Successfully Saved");
 				reset();
 			})
@@ -31,6 +76,8 @@ var healthseeker = new (function() {
 	}
 
 	self.update = function() {
+		var data = storage.getHealthSeeker();
+
 		var name = $("#name").val();
 		var mobile = $("#mobile").val();
 		var age = $("#age").val();
@@ -43,6 +90,7 @@ var healthseeker = new (function() {
 		data.age = age;
 		hsPresenter.update(id, data)
 			.then(res => {
+				storage.resetHealthSeeker();
 				alert("Successfully Updated");
 				reset();
 			})
